a24bd6c2f8adcb941d25beb0cdde7e2e29c963e0
MasturFYC <mastur.st12@gmail.com>
2021-04-05 16:36:09 +0700

    test-10

--- next.config.js
+++ next.config.js
@@ -33,3 +33,14 @@ module.exports = {
     ]
   },
 }
+
+module.exports = {
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+
+    return config
+  },
+}
